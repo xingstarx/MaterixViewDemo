f0655e71dbe80cd6f3b47ed1262875b5ae2c5bb2
xingxing <xxx823952375@gmail.com>
2016-07-17 16:49:07 +0800

    only use ScaleGestureDetector

--- app/src/main/java/star/github/com/materixviewdemo/MainActivity.java
+++ app/src/main/java/star/github/com/materixviewdemo/MainActivity.java
@@ -1,32 +1,106 @@
 package star.github.com.materixviewdemo;
 
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, ScaleGestureDetector.OnScaleGestureListener {
 
-    private MaterixImageView mMaterixImageView;
+    private static final String TAG = "MainActivity";
+    private ImageView mMaterixImageView;
+    private Matrix mBaseMatrix = new Matrix();
+
+    private ScaleGestureDetector mScaleGestureDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMaterixImageView = (MaterixImageView) findViewById(R.id.materix_image_view);
+        mMaterixImageView = (ImageView) findViewById(R.id.materix_image_view);
+        mMaterixImageView.setScaleType(ImageView.ScaleType.MATRIX);
+        mMaterixImageView.setOnTouchListener(this);
 
+        mScaleGestureDetector = new ScaleGestureDetector(this, this);
         Picasso.with(this)
                 .load("http://pbs.twimg.com/media/Bist9mvIYAAeAyQ.jpg")
                 .fit().centerInside()
                 .into(mMaterixImageView, new Callback() {
                     @Override
                     public void onSuccess() {
-                        mMaterixImageView.update();
+                        update();
                     }
 
                     @Override
                     public void onError() {
                     }
                 });
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        boolean flag = mScaleGestureDetector.onTouchEvent(event);
+        Log.e(TAG, "flag == " + flag);
+        return flag;
+    }
+
+    @Override
+    public boolean onScale(ScaleGestureDetector detector) {
+        float scaleFactor = detector.getScaleFactor();
+        Log.e(TAG, "onScale == sacleFactor == " + scaleFactor);
+        if (Float.isNaN(scaleFactor) || Float.isInfinite(scaleFactor)) {
+            return false;
+        }
+        mBaseMatrix.postScale(scaleFactor, scaleFactor, detector.getFocusX(), detector.getFocusY());
+        mMaterixImageView.setImageMatrix(mBaseMatrix);
+        return true;
+    }
+
+    public void update() {
+        if (!hasDrawable()) {
+            return;
+        }
+        mBaseMatrix.postTranslate((getImageWidth() - getDrawableWidth()) / 2, (getImageHeight() - getDrawableHeight()) / 2);
+        mMaterixImageView.setImageMatrix(mBaseMatrix);
+    }
+
+    private boolean hasDrawable() {
+        if (mMaterixImageView != null && mMaterixImageView.getDrawable() != null && mMaterixImageView.getDrawable() instanceof BitmapDrawable) {
+            return true;
+        }
+        return false;
+    }
+
+    private float getImageWidth() {
+        return mMaterixImageView.getMeasuredWidth() - mMaterixImageView.getPaddingLeft() - mMaterixImageView.getPaddingRight();
+    }
+
+    private float getImageHeight() {
+        return mMaterixImageView.getMeasuredHeight() - mMaterixImageView.getPaddingTop() - mMaterixImageView.getPaddingBottom();
+    }
+
+    private float getDrawableWidth() {
+        return mMaterixImageView.getDrawable().getIntrinsicWidth();
+    }
+
+    private float getDrawableHeight() {
+        return mMaterixImageView.getDrawable().getIntrinsicHeight();
+    }
+
+    @Override
+    public boolean onScaleBegin(ScaleGestureDetector detector) {
+        return true;
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
     }
 }
